c31e0d90840c35134f445f0210482f5b1e040e6e
unte2002 <101990178+unte2002@users.noreply.github.com>
2024-11-20 21:22:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
         english_file = process_file(english_file)
         english_file = process_file(german_file)
 
-        processed_file_list.append(template_end + english_file + template_mid + german_file + template_start)
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
     return processed_file_list
 
 
